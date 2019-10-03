394948f3687f85c27254050dbe59c223e850f46a
Hwm5114 <55036627+Hwm5114@users.noreply.github.com>
2019-10-03 14:57:26 -0400

    Add telemetry to see Motor Power
    Testing to see if we can get full power while driving field oriented when stick is maxed out.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MecanumDrive.java
@@ -34,6 +34,16 @@ public class MecanumDrive {
         leftRearPower = magnitude * Math.sin(angle - Math.PI/4) + pivot;
         rightFrontPower = magnitude * Math.sin(angle - Math.PI/4) - pivot;
         rightFrontPower = magnitude * Math.cos(angle - Math.PI/4) - pivot;
+        
+        
+        //need to check if we get full power on the robot wheels after this transformation. 
+        //if the driver asks for full power and then we adjust with orientation we might need to recalculate the magnitude first.
+        //the PS4 joystick aren't radial.   So the magnitude at 45 degrees on the stick is 1.41 at 30 is only 1.15
+        // if the stick is at 30 and the orientation adds another 15 degrees we are short on the intended magnitude.
+        // I think we need to calculate the percent of full scale at the joystick angle and then use that as the magnitude at the
+        // corrected angle.
+        // -  to test is this is an issue we just need to display the motor power while test drivinig.
+        
 
         scale = Math.max(
                 Math.max(Math.abs(leftFrontPower), Math.abs(leftRearPower)),
@@ -51,6 +61,16 @@ public class MecanumDrive {
         hw.leftRearDrive.setPower(leftRearPower);
         hw.rightFrontDrive.setPower(rightFrontPower);
         hw.rightRearDrive.setPower(rightRearPower);
+        
+        
+        telemetry.addData("Left Front Power", leftFrontPower);
+        telemetry.addData("Left Rear Power" , leftRearPower);
+        telemetry.addData("Right Front Power", rightFrontPower);
+        telemetry.addData("Right Rear Power", rightRearPower);
+        updateTelemetry(telemetry);
+                          
+        
+        
     }
 
     public void pivotToAngle(double power, double angle){
