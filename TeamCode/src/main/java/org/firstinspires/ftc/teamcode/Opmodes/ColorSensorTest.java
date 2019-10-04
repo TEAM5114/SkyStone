1ff27621c2f624e023d21eddececd7511bf28c98
mfanone <mark@martin-fanone.org>
2019-10-04 15:14:09 -0400

    Add code to change Robot Controller background color to better understand what color is being detected.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/ColorSensorTest.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.Opmodes;
 
+import android.app.Activity;
 import android.graphics.Color;
+import android.view.View;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,30 +13,47 @@ import org.firstinspires.ftc.teamcode.Robot.Robot;
 @TeleOp(name = "Color sensor", group = "Test")
 public class ColorSensorTest extends LinearOpMode {
     float[] hsvValues = new float[3];
+    final float values[] =hsvValues;
     Robot robot = new Robot();
 
+    View relativeLayout;
+
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         robot.init(hardwareMap);
 
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
         waitForStart();
+        try {
+            while (opModeIsActive()) {
+                NormalizedRGBA colors = robot.getColors();
+                Color.colorToHSV(colors.toColor(), hsvValues);
 
-        while (opModeIsActive()){
-            NormalizedRGBA colors = robot.getColors();
-            Color.colorToHSV(colors.toColor(), hsvValues);
+                telemetry.addLine()
+                        .addData("H", "%.3f", hsvValues[0])
+                        .addData("S", "%.3f", hsvValues[1])
+                        .addData("V", "%.3f", hsvValues[2]);
+                telemetry.addLine()
+                        .addData("a", "%.3f", colors.alpha)
+                        .addData("r", "%.3f", colors.red)
+                        .addData("g", "%.3f", colors.green)
+                        .addData("b", "%.3f", colors.blue);
 
-            telemetry.addLine()
-                    .addData("H", "%.3f", hsvValues[0])
-                    .addData("S", "%.3f", hsvValues[1])
-                    .addData("V", "%.3f", hsvValues[2]);
-            telemetry.addLine()
-                    .addData("a", "%.3f", colors.alpha)
-                    .addData("r", "%.3f", colors.red)
-                    .addData("g", "%.3f", colors.green)
-                    .addData("b", "%.3f", colors.blue);
-
-            telemetry.update();
+                telemetry.update();
+                relativeLayout.post(new Runnable() {
+                    public void run() {
+                        relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                    }
+                });
+            }
+        } finally {
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.WHITE);
+                }
+            });
         }
     }
-
 }
